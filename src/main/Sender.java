408d1485167b0a85b986ba0d08f02e246a90de66
icunhacorrea <iago.cunhac@gmail.com>
2020-10-05 14:28:29 -0300

    Records em 1000

--- src/main/Sender.java
+++ src/main/Sender.java
@@ -24,7 +24,7 @@ public class Sender extends Thread {
         try {
             while (running) {
 		synchronized (records) {
-			if (records.size() >= 100) {
+			if (records.size() >= 1000) {
 			    send();
 			    records.clear();
 			}

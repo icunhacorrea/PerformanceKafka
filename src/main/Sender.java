38587dc7991cc01650104d2ca615cd6f5c229e35
icunhacorrea <iago.cunhac@gmail.com>
2020-11-08 20:06:46 -0300

    ...

--- src/main/Sender.java
+++ src/main/Sender.java
@@ -3,9 +3,7 @@ package main;
 import recordutil.src.main.Record;
 
 import java.io.ObjectOutputStream;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -23,25 +21,15 @@ public class Sender extends Thread {
     @Override
     public void run() {
         boolean running = true;
-
-        SocketAddress sockAddr = new InetSocketAddress("172.21.0.8", 6666);
-        Socket socket = new Socket();
-        try {
-            socket.setSendBufferSize(Integer.MAX_VALUE);
-            socket.connect(sockAddr, 10000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         try {
             while (running) {
                 synchronized (records) {
                     if (finished.get() == true) {
-                        send(socket, sockAddr);
+                        send();
                         running = false;
                     }
                     if (records.size() >= 128) {
-                        send(socket, sockAddr);
+                        send();
                         records.clear();
                     }
                 }
@@ -51,10 +39,11 @@ public class Sender extends Thread {
         }
     }
 
-    public void send(Socket socket, SocketAddress sockAddr) {
+    public void send() {
         try {
             //Socket socket = new Socket("monitor1", 6666);
-            //socket.setSendBufferSize(Integer.MAX_VALUE);
+            Socket socket = new Socket("172.21.0.8", 6666);
+            socket.setSendBufferSize(Integer.MAX_VALUE);
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(records);
         } catch (Exception e) {

3de6c18c33582fc97653a1cff002e6c1eb54f87b
icunhacorrea <iago.cunhac@gmail>
2020-11-24 18:29:23 -0300

    ...

--- src/main/Sender.java
+++ src/main/Sender.java
@@ -28,7 +28,7 @@ public class Sender extends Thread {
                         makeSends();
                         running = false;
                     }
-                    if (records.size() >= 256) {
+                    if (records.size() >= 512) {
                         makeSends();
                         records.clear();
                     }
@@ -44,7 +44,7 @@ public class Sender extends Thread {
         try {
             Socket socket = new Socket("172.21.0.8", 6666);
             //socket.setSendBufferSize(Integer.MAX_VALUE);
-            socket.setSoTimeout(500);
+            socket.setSoTimeout(2000);
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(records);
         } catch (Exception ex) {

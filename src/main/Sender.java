ab807b8738897cfd20b5cf0b0f98dff8208cf11d
icunhacorrea <iago.cunhac@gmail>
2020-11-28 14:29:20 -0300

    ...

--- src/main/Sender.java
+++ src/main/Sender.java
@@ -44,7 +44,7 @@ public class Sender extends Thread {
         try {
             Socket socket = new Socket("172.21.0.8", 6666);
             //socket.setSendBufferSize(Integer.MAX_VALUE);
-            socket.setSoTimeout(1000);
+            socket.setSoTimeout(5000);
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(records);
         } catch (Exception ex) {

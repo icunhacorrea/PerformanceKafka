17b4cd37851e2059fb983e976999a7c2177c51c9
icunhacorrea <iago.cunhac@gmail>
2020-12-01 14:55:55 -0300

    ...

--- src/execute.sh
+++ src/execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp "ProducerPerformance.jar:../jars/*" main.ProducerPerformance $1 $2 $3 $4
+java -cp "ProducerPerformance.jar:../jars/*" main.ProducerPerformance $1 $2 $3 $4 -Xms2048M -Xmx4096M
 
